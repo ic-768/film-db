7262352be15830384ae9cec9ad474dd26c84d23c
Ioannis Chrysostomakis <ic768@protonmail.com>
2023-08-04 16:28:52 +0300

    Extract movie detail component

--- src/pages/movie/MoviePage.tsx
+++ src/pages/movie/MoviePage.tsx
@@ -40,6 +40,12 @@ const MoviePage = () => {
     fetchMovie();
   }, [id]);
 
+  const MovieDetail = ({ name, detail }: { name: string; detail: string }) => (
+    <p className="text-gray-600 mb-4">
+      <strong>{name}:</strong> {detail}
+    </p>
+  );
+
   return movie ? (
     <div className="container mx-auto p-4">
       <div className="max-w-3xl mx-auto bg-white rounded-lg relative">
@@ -51,76 +57,37 @@ const MoviePage = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <img className="w-full" src={movie.Poster} alt={movie.Title} />
           <div className="px-6 py-4">
-            <div className="font-bold text-3xl mb-2">{movie.Title}</div>
-            <p className="text-gray-600 mb-4">
-              <strong>Year:</strong> {movie.Year}
-            </p>
-            <p className="text-gray-600 mb-4">
-              <strong>Director:</strong> {movie.Director}
-            </p>
-            <p className="text-gray-600 mb-4">
-              <strong>Genre:</strong> {movie.Genre}
-            </p>
-            <p className="text-gray-600 mb-4">
-              <strong>Runtime:</strong> {movie.Runtime}
-            </p>
-            <p className="text-gray-600 mb-4">
-              <strong>Rated:</strong> {movie.Rated}
-            </p>
-            <p className="text-gray-600 mb-4">
-              <strong>IMDb Rating:</strong> {movie.imdbRating}
-            </p>
-            <p className="text-gray-600 mb-4">
-              <strong>IMDb Votes:</strong> {movie.imdbVotes}
-            </p>
-            <p className="text-gray-600 mb-4">
-              <strong>Plot:</strong> {movie.Plot}
-            </p>
+            <div className="font-bold text-3xl mb-2 text-gray-800">
+              {movie.Title}
+            </div>
+            <MovieDetail name="Year" detail={movie.Year} />
+            <MovieDetail name="Director" detail={movie.Director} />
+            <MovieDetail name="Genre" detail={movie.Genre} />
+            <MovieDetail name="Runtime" detail={movie.Runtime} />
+            <MovieDetail name="Rated" detail={movie.Rated} />
+            <MovieDetail name="IMDb Rating" detail={movie.imdbRating} />
+            <MovieDetail name="IMDb Votes" detail={movie.imdbVotes} />
+            <MovieDetail name="Plot" detail={movie.Plot} />
           </div>
         </div>
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">Additional Details</div>
-          <div className="grid grid-cols-2 gap-4">
-            <p className="text-gray-600">
-              <strong>Actors:</strong> {movie.Actors}
-            </p>
-            <p className="text-gray-600">
-              <strong>Language:</strong> {movie.Language}
-            </p>
-            <p className="text-gray-600">
-              <strong>Country:</strong> {movie.Country}
-            </p>
-            <p className="text-gray-600">
-              <strong>Released:</strong> {movie.Released}
-            </p>
-            <p className="text-gray-600">
-              <strong>Awards:</strong> {movie.Awards}
-            </p>
-            <p className="text-gray-600">
-              <strong>Metascore:</strong> {movie.Metascore}
-            </p>
-            <p className="text-gray-600">
-              <strong>Production:</strong> {movie.Production}
-            </p>
-            <p className="text-gray-600">
-              <strong>Box Office:</strong> {movie.BoxOffice}
-            </p>
-            <p className="text-gray-600">
-              <strong>DVD:</strong> {movie.DVD}
-            </p>
-            <p className="text-gray-600">
-              <strong>Website:</strong> {movie.Website}
-            </p>
-            <p className="text-gray-600">
-              <strong>Type:</strong> {movie.Type}
-            </p>
-            <p className="text-gray-600">
-              <strong>Response:</strong> {movie.Response}
-            </p>
-            <p className="text-gray-600">
-              <strong>Writer:</strong> {movie.Writer}
-            </p>
-            {/* Add more details as needed */}
+          <div className="font-bold text-xl mb-2 text-gray-800">
+            Additional Details
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2">
+            <MovieDetail name="Actors" detail={movie.Actors} />
+            <MovieDetail name="Language" detail={movie.Language} />
+            <MovieDetail name="Country" detail={movie.Country} />
+            <MovieDetail name="Released" detail={movie.Released} />
+            <MovieDetail name="Awards" detail={movie.Awards} />
+            <MovieDetail name="Metascore" detail={movie.Metascore} />
+            <MovieDetail name="Production" detail={movie.Production} />
+            <MovieDetail name="Box Office" detail={movie.BoxOffice} />
+            <MovieDetail name="DVD" detail={movie.DVD} />
+            <MovieDetail name="Website" detail={movie.Website} />
+            <MovieDetail name="Type" detail={movie.Type} />
+            <MovieDetail name="Response" detail={movie.Response} />
+            <MovieDetail name="Writer" detail={movie.Writer} />
           </div>
         </div>
       </div>
